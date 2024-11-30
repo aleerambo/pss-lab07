02e26affd3fba80bf364e4e8835c63f3ef1dbccf
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-11-30 12:48:25 +0100

    UPDATE AbstractBankAccount class

---
+++ 01-abstract-classes/src/main/java/it/unibo/inheritance/AbstractBankAccount.java
@@ -0,0 +1,73 @@
+package it.unibo.inheritance;
+
+import it.unibo.inheritance.api.AccountHolder;
+import it.unibo.inheritance.api.BankAccount;
+
+public abstract class AbstractBankAccount implements BankAccount {
+    public static final double ATM_TRANSACTION_FEE = 1;
+    public static final double MANAGEMENT_FEE = 5;
+
+    private final AccountHolder holder;
+    private double balance;
+    private int transactions;
+
+    public AbstractBankAccount(AccountHolder accountHolder, double balance, int transactions) {
+        this.holder = accountHolder;
+        this.balance = balance;
+        this.transactions = 0;
+    }
+
+    public void chargeManagementFees(int id) {
+        if (checkUser(id)) {
+            this.balance -= AbstractBankAccount.MANAGEMENT_FEE;
+            resetTransactions();
+        }
+    }
+
+    public void deposit(int id, double amount) {
+        this.transactionOp(id, amount);
+    }
+
+    public void depositFromATM(int id, double amount) {
+        this.deposit(id, amount - AbstractBankAccount.ATM_TRANSACTION_FEE);
+    }
+
+    public AccountHolder getAccountHolder() {
+        return holder;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public int getTransactionsCount() {
+        return this.transactions;
+    }
+
+    public void withdraw(int id, double amount) {
+        this.transactionOp(id, -amount);
+    }
+
+    public void withdrawFromATM(int id, double amount) {
+        this.withdraw(id, amount + AbstractBankAccount.ATM_TRANSACTION_FEE);
+    }
+
+    protected boolean checkUser(final int id) {
+        return this.getAccountHolder().getUserID() == id;
+    }
+
+    protected void incrementTransactions() {
+        this.transactions++;
+    }
+
+    protected void resetTransactions() {
+        this.transactions = 0;
+    }
+
+    private void transactionOp(final int id, final double amount) {
+        if (checkUser(id)) {
+            this.balance += amount;
+            this.incrementTransactions();
+        }
+    }
+}
