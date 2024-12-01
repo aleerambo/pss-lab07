0cdf8fb1699d8fbec763c1e09e716fa9423e7494
Alessandro Rambaldi <alessandro.rambaldi@studio.unibo.it>
2024-12-01 09:01:18 +0100

    UPDATE ClassicBankAccount with constructor

--- 01-abstract-classes/src/main/java/it/unibo/inheritance/impl/ClassicBankAccount.java
+++ 01-abstract-classes/src/main/java/it/unibo/inheritance/impl/ClassicBankAccount.java
@@ -1,7 +1,10 @@
 package it.unibo.inheritance.impl;
 
 import it.unibo.inheritance.AbstractBankAccount;
+import it.unibo.inheritance.api.AccountHolder;
 
 public class ClassicBankAccount extends AbstractBankAccount {
-    
+    public ClassicBankAccount(final AccountHolder accountHolder, final double balance) {
+        super(accountHolder, balance);
+    }
 }
